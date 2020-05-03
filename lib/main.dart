bedf09e900f7821e3c78ae331b478e45c4be495d
RyuuKenshi <ryuukenshi@users.noreply.github.com>
2020-05-03 22:59:31 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tensorflow_lite_flutter/screens/detect_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Waste Segregation Classifier',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DetectScreen(title: 'Waste Classifier'),
+    );
+  }
+}
